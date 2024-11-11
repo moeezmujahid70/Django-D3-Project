ab4add8e86f5fe1d65da842de2cf3cb6a5ef3971
Moeez Mujahid <moeezmujahid70@gmail.com>
2024-11-12 00:24:53 +0100

    added urls paths for 1. token generation 2. chart api's

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,13 @@ from django.urls import include
 from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from charts import views as chart_views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -20,6 +27,15 @@ urlpatterns = [
     path("users/", include("d3project.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path('chart/', chart_views.sample_chart_view, name='chart'),
+   
+    path('api/', include('charts.api_urls')), # REST API
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh token
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),  # Verify token
+    path('api/token/current-user/', chart_views.generate_token_for_logged_in_user, name='token_for_current_user'),
+
+    
     # ...
     # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
